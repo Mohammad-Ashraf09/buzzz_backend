1bd0e3e1d63f41e16c95988aafda528cb87a6b5d
Mohammad Ashraf <mohd.ashraf@tothenew.com>
2023-10-07 00:26:44 +0530

    prod error: fixing error attempt 9

--- app.js
+++ app.js
@@ -60,6 +60,13 @@ app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/notifications", notificationRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
+
 
 // get all the users from social collection name of database
 // var database;
